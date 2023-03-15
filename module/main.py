e4206b6501190d7779a3f17c6413d1ffb28e3579
ahvcxa <inanbatuhan5@gmail.com>
2023-03-15 16:32:15 +0300

    module/mod.py and module/main.py added.

---
+++ module/main.py
@@ -0,0 +1,14 @@
+import mod
+
+#result = help(mod)
+#print(result)
+
+#result = help(mod.Developer)
+#print(result)
+
+result = mod.info["name"]
+
+print(result)
+
+p = mod.Developer()
+p.into()
---
+++ module/mod.py
@@ -0,0 +1,18 @@
+"""
+This module shows Batuhan's informations
+"""
+print("Mod created")
+
+info = {
+    "name" : "Batuhan",
+    "Surname" : "Inan",
+    "job" : "Software Developer"
+}
+
+class Developer:
+    def into(self):
+        """
+        This function introduces programmers
+        """
+        print("Hi,my name'is Batuhan")
+        
